14b76717d7fbdd2914871b870859d88d7aceb145
Modesto Mejia <modesto,mejia@conceptomovil.com>
2021-11-03 04:21:34 -0600

    push camel-file

---
+++ camel-file/src/main/java/com/camel/file/camelfile/CamelFileApplication.java
@@ -0,0 +1,13 @@
+package com.camel.file.camelfile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CamelFileApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CamelFileApplication.class, args);
+	}
+
+}
---
+++ camel-file/src/test/java/com/camel/file/camelfile/CamelFileApplicationTests.java
@@ -0,0 +1,13 @@
+package com.camel.file.camelfile;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CamelFileApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
