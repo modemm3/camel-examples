e22ac21cc8c530f7cfaca8a2dbffe1ec029aa4fa
Modesto Mejia <modesto,mejia@conceptomovil.com>
2021-11-03 04:38:24 -0600

    push camel multicast

---
+++ camel-multicast/src/test/java/com/camel/multicast/camelmulticast/CamelMulticastApplicationTests.java
@@ -0,0 +1,13 @@
+package com.camel.multicast.camelmulticast;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CamelMulticastApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
